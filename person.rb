1d4e67873534cf3bc05df5a41f9f23143167d497
Ben Kiboma <omayioben@gmail.com>
2022-08-22 22:51:49 +0300

    Create class person Create constructors Create Getters and Setters

---
+++ person.rb
@@ -0,0 +1,24 @@
+class Person
+  def initialize(name = "Unknown",age,parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def name = (value)
+    @name = value
+  end
+end
